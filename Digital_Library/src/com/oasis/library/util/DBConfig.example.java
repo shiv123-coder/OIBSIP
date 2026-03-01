80673ce9d3d48ba2641c24163ef8d17cd04fbbac
Shiv_Core <shivashankrmali7@gmail.com>
2026-03-01 16:27:30 +0530

    Stop tracking DBConfig; add example config

---
+++ Digital_Library/src/com/oasis/library/util/DBConfig.example.java
@@ -0,0 +1,7 @@
+package com.oasis.library.util;
+
+public class DBConfig {
+    public static final String URL = "jdbc:mysql://localhost:3306/library_db";
+    public static final String USER = "YOUR_MYSQL_USERNAME";
+    public static final String PASSWORD = "YOUR_MYSQL_PASSWORD";
+}
